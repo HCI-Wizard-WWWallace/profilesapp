4608067ee5a83234de75a28a9941d7269ee1e538
HCI-Wizard-WWWallace <256935632+HCI-Wizard-WWWallace@users.noreply.github.com>
2026-09-20 09:51:33 -0400

    displaying user profile

---
+++ amplify/auth/post-confirmation/handler.ts
@@ -0,0 +1,46 @@
+import type { PostConfirmationTriggerHandler } from 'aws-lambda';
+import { Amplify } from 'aws-amplify';
+import { generateClient } from 'aws-amplify/api';
+import { createUserProfile } from './graphql/mutations';
+import { env } from '\$amplify/env/post-confirmation';
+
+Amplify.configure(
+  {
+    API: {
+      GraphQL: {
+        endpoint: env.AMPLIFY_DATA_GRAPHQL_ENDPOINT,
+        region: env.AWS_REGION,
+        defaultAuthorizationMode: 'iam'
+      }
+    }
+  },
+  {
+    Auth: {
+      credentialsProvider: {
+        getCredentialsAndIdentityId: async () => ({
+          credentials: {
+            accessKeyId: env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+            sessionToken: env.AWS_SESSION_TOKEN
+          }
+        })
+      }
+    }
+  }
+);
+
+const client = generateClient();
+
+export const handler: PostConfirmationTriggerHandler = async (event) => {
+  await client.graphql({
+    query: createUserProfile,
+    variables: {
+      input: {
+        email: event.request.userAttributes.email,
+        profileOwner: `${event.request.userAttributes.sub}::${event.userName}`
+      }
+    }
+  });
+
+  return event;
+};
